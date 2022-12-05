eef5b87bb7bd4acdb1badc80578dd49ef716b90f
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-12-05 03:06:29 -0800

    DBStorage - States and Cities

---
+++ models/engine/db_storage.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python3
+""" Defines the DBStorage engine."""
+from os import getenv
+from models.base_model import Base
+from models.base_model import BaseModel
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+from sqlalchemy import create_engine
+from sqlalchemy.orm import relationship
+from sqlalchemy.orm import scoped_session
+from sqlalchemy.orm import sessionmaker
+
+
+class DBStorage:
+    """This represents a database storage engine.
+
+    Attributes:
+        __engine (sqlalchemy Engine): The working SQLAlchemy engine.
+        __session (sqlalchemy Session): The working SQLAlchemy session.
+    """
+    __engine = None
+    __session = None
+
+    def __init__(self):
+        """Initialize a DBStorage instance."""
+        user = getenv("HBNB_MYSQL_USER")
+        passwd = getenv("HBNB_MYSQL_PWD")
+        db = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
+
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                      .format(user, passwd, host, db), 
+                                      pool_pre_ping=True)
+
+        if env == "test":
+            Base.metadata.drop_all(self.__engine)
+
+    def all(self, cls=None):
+        """Returns a dictionary."""
+        dic = {}
+        if cls:
+            if type(cls) is str:
+                cls = eval(cls)
+            query = self.__session.query(cls)
+            for elem in query:
+                key = "{}.{}".format(type(elem).__name__, elem.id)
+                dic[key] = elem
+        else:
+            lista = [State, City, User, Place, Review, Amenity]
+            for index in lista:
+                query = self.__session.query(item)
+                for elem in query:
+                    key = "{}.{}".format(type(elem).__name__, elem.id)
+                    dic[key] = elem
+        return (dic)
+
+    def new(self, obj):
+        """ Adds a new element in the table."""
+        self.__session.add(obj)
+
+    def save(self):
+        """ Deletes an element in the table."""
+        if obj:
+            self.session.delete(obj)
+
+    def reload(self):
+        """Configuration."""
+        Base.metadata.create_all(self.__engine)
+        sec = sessionmaker(bind=self.__engine, expire_on_commit=Fale)
+        Session = scoped_session(sec)
+        self.__session = Session()
+
+    def close(self):
+        """ Calls remove()."""
+        self.__session.close()
