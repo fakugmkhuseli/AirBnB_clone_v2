4153ba1033a87397be710a77c8b7f8ca2204f7a6
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-12-06 06:00:47 -0800

    DBStorage - Review

--- models/review.py
+++ models/review.py
@@ -3,7 +3,6 @@
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
-from sqlalchemy.orm import relationship
 
 
 class Review(BaseModel, Base):
