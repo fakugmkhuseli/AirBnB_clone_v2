a615f83174d7125354761b52baf9f945d64b5cec
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-12-06 06:48:46 -0800

    DBStorage - Amenity

--- models/amenity.py
+++ models/amenity.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ State class for HBNB project """
-from models.base_model import BaseModel, Base
+from models.base_model import Base
+from models.base_model import BaseModel
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 from models.place import place_amenity
