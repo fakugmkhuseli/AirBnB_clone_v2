df2e71b818695d82cc46fa1a8ce8b0c8662d38d3
Mkhuseli Faku <fakugmkhuseli@gmail.com>
2022-12-22 13:38:08 +0000

    Preparing web servers

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-# Sets up your web servers for the deployment of web_static.
+# Sets up web servers for the deployment of web_static.
 
 apt-get update
 apt-get install -y nginx
@@ -25,7 +25,7 @@ printf %s "server {
     }
 
     location /redirect_me {
-        return 301 http://cuberule.com/:
+        return 301 http://cuberule.com/;
     }
 
     error_page 404 /404.html;
